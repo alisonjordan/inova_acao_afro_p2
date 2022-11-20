47cb8861ba01bc0aed5fda027b863480f80f8eac
alisonjordan <alisonjor@gmail.com>
2022-11-20 07:56:25 -0300

    criação do model Log e suas validações

---
+++ app/models/log.rb
@@ -0,0 +1,5 @@
+class Log < ApplicationRecord
+
+    alidates :comentario_id, presence: true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_221414) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_20_101607) do
   create_table "ators", force: :cascade do |t|
     t.string "nome"
     t.integer "ano_nascimento"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "logs", force: :cascade do |t|
+    t.integer "comentario_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
